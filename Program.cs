09cd12d24da3968784c55d4f2ad44139471326f2
Jack Gladding (1000142926) <GLADJ3@student.op.ac.nz>
2026-09-22 11:10:36 +1200

    Added JacksMethod currency converter

--- Program.cs
+++ Program.cs
@@ -6,5 +6,33 @@
         {
             Console.WriteLine("This is the first line");
         }
+
+	static void JacksMethod()
+	{
+	    const double AUD = 0.8085, USD = 0.8272, GBP = 0.5457, YEN = 76.23, EUR = 0.6297;
+
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.Write("Enter amount to convert (NZD): ");
+            double NZD = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"{ (NZD * AUD):C}");
+            Console.WriteLine($"{ (NZD * USD):C}");
+            Console.WriteLine("£" + $"{ (NZD * GBP):F2}");
+            Console.WriteLine("¥" + $"{(NZD * YEN):F2}");
+            Console.WriteLine("€" + $"{(NZD * YEN):F2}");
+
+            Console.ReadLine();
+
+            Console.Write("=" + $"{(MilesToKm()):F2}" + " km\n");
+
+            Console.ReadLine();
+
+            Console.Write("Total Price: " + $"{(TicketPrices()):C}");
+
+            Console.ReadLine();
+
+	}
+
     }
 }
