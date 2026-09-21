9ab0541576e17c09bd54051842cdf5b90ae72023
Dirk Van Huyssteen (1000113190) <VANHDW1@student.op.ac.nz>
2026-09-22 11:19:10 +1200

    werners methods

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace new_project_test
+﻿using System;
+
+namespace new_project_test
 {
     internal class Program
     {
@@ -36,3 +38,83 @@
 
     }
 }
+    
+
+
+    /// <summary>
+    /// Werner workking stuff
+    /// </summary>
+    internal class Program
+    {
+        static void Menusystem(string[] args)
+        {
+            int Tasks;
+            do
+            {
+
+                Console.WriteLine("Enter the numbers to choose the tasks: ");
+                Console.WriteLine("1. Task 1 \n2. Task 2 \n3. Task3 \n4. Task 4 \n 0. Exit menu");
+
+
+                string temp = Console.ReadLine();
+                Tasks = Convert.ToInt32(temp);
+                Console.Clear();
+                switch (Tasks)
+                {
+                    case 0:
+                        Console.WriteLine("Exiting system...");
+                        break;
+
+                    case 1:
+                        Task1();
+                        break;
+
+                    case 2:
+                        Task2();
+                        break;
+
+                    case 3:
+                        Task3();
+                        break;
+
+                    case 4:
+                        Task4();
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid choice. Try again.");
+                        break;
+                }
+                Console.Clear();
+            } while (Tasks != 0);
+        }
+        public static void Task1()
+        {
+            Console.WriteLine("This is task one");
+            Console.WriteLine("Press any key to exit");
+            Console.ReadLine();
+        }
+        public static void Task2()
+        {
+            Console.WriteLine("This is task two");
+            Console.WriteLine("Press any key to exit");
+            Console.ReadLine();
+        }
+        public static void Task3()
+        {
+            Console.WriteLine("This is task three");
+            Console.WriteLine("Press any key to exit");
+            Console.ReadLine();
+        }
+        public static void Task4()
+        {
+            Console.WriteLine("This is task four");
+            Console.WriteLine("Press any key to exit");
+            Console.ReadLine();
+        }
+
+
+
+    }
+}
+
