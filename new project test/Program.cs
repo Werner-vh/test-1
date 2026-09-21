26fac11260d22d15b67c789f9878295e384c8c1c
Dirk Van Huyssteen (1000113190) <VANHDW1@student.op.ac.nz>
2026-09-22 10:42:02 +1200

    firt commit

---
+++ new project test/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace new_project_test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is the first line");
+        }
+    }
+}
